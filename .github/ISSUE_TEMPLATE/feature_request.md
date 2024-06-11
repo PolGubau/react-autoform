f4e6c06b8798d3c0ff1408ed69caf0d281be8692
Pol Gubau Amores <gubaupol@gmail.com>
2024-06-11 22:52:30 +0200

    first commit

---
+++ .github/ISSUE_TEMPLATE/feature_request.md
@@ -0,0 +1,15 @@
+---
+name: Feature creation
+about: New feature for react-autoform
+---
+
+- [ ] I have searched the [Issues](https://github.com/polgubau/react-autoform/issues) to see if this bug has already been reported
+- [ ] I have tested the latest version
+
+## Summary
+
+Describe how it should work, and provide examples of the solution, which might include screenshots or code snippets.
+
+## Context
+
+What are you trying to accomplish? How is your use case affected by not having this feature?
---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import Api from "./examples/Api";
+import Array from "./examples/Array";
+import Basics from "./examples/Basics";
+import ConfirmPassword from "./examples/ConfirmPassword";
+import Controlled from "./examples/Controlled";
+import Dependencies from "./examples/Dependencies";
+import InputWithoutLabel from "./examples/InputWithoutLabel";
+import SubObject from "./examples/SubObject";
+
+function App() {
+  return (
+    <>
+      <div className="mx-auto my-6 max-w-lg space-y-8">
+        <Basics />
+        <InputWithoutLabel />
+        <SubObject />
+        <Controlled />
+        <ConfirmPassword />
+        <Api />
+        <Array />
+        <Dependencies />
+      </div>
+    </>
+  );
+}
+
+export default App;
